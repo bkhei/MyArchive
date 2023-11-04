b0d574825eff09378f20c1e3941c44d4652c8bc4
YVega <yolanda.n.vega@gmail.com>
2023-11-04 16:57:19 -0400

    Initialized Parse SDK with new Back4App project

--- MyArchive/MyArchive/AppDelegate.swift
+++ MyArchive/MyArchive/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ParseSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ParseSwift.initialize(applicationId: "YnERzK1pPzSarWbUYSE8sUzAeE6nok4MW16FD5eA",
+                              clientKey: "VPr0raUzUcgE1Ll7Iwcf3V9UrSoNjwex8iUSqrqV",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        
         return true
     }
 
