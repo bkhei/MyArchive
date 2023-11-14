39d2b9cbcce72087777c48e20a9fec3c04875c11
YVega <yolanda.n.vega@gmail.com>
2023-11-13 23:13:19 -0500

    Added navigation prepare function to Home, SearchResult, and Library and connected them to StoryCell

--- MyArchive/MyArchive/Story Lists (Home, Search Results, Library)/LibraryViewController.swift
+++ MyArchive/MyArchive/Story Lists (Home, Search Results, Library)/LibraryViewController.swift
@@ -21,14 +21,30 @@ class LibraryViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // Prepare for segue and send data to detail view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell),
+           let DVC = segue.destination as? DetailViewController {
+            let tappedStory = stories[indexPath.row]
+            DVC.story = tappedStory
+        }
     }
-    */
 
+}
+// Conforming view controller to UITableViewDataSource -- INCOMPLETE
+extension LibraryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StoryCell", for: indexPath) as? StoryCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: stories[indexPath.row])
+        return cell
+    }
+    
+    
 }
