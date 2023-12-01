8d3faa2b1998587752a4ded48acf7372f54d15cc
YVega <yolanda.n.vega@gmail.com>
2023-11-30 21:27:34 -0500

    Commiting unfinished functionality to adding story to library

--- MyArchive/MyArchive/Detail/DetailViewController.swift
+++ MyArchive/MyArchive/Detail/DetailViewController.swift
@@ -20,6 +20,19 @@ class DetailViewController: UIViewController {
         tableView.dataSource = self
     }
     
+    func checkLibrary(_ user: User) -> Bool {
+        let story = delegate?.requestStoryProperty()
+        if (user.library.isEmpty) {
+            return false
+        } else {
+            for s in user.library {
+                if (story == s) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
 
     // Prepare for segue and send data to read view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -51,5 +64,50 @@ extension DetailViewController: UITableViewDataSource, DetailCellDelegate {
     }
     func reloadTable() {
         self.tableView.reloadData()
+    }
+}
+// Tap gesture funcitons
+extension ReadViewController {
+    @IBAction func onViewTapped(_ sender: Any) {
+        print("Row 0 tapped, ending editing")
+        view.endEditing(true)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func didTapAddLibrary(_ sender: UITapGestureRecognizer) {
+        if let tappedIMG = sender.view as? UIImageView {
+            // Accessing the tapped image (there is only 1)
+            // Add current story to current user's library
+            // Checking whether in library or not
+            let inLibrary = checkLibrary(User.current!)
+            let story = delegate?.requestStoryProperty()
+            var currentUser = User.current!
+            // Add library button/gesture tapped
+            if (inLibrary == true) {
+                // already in library, tapped to remove
+                currentUser.library = currentUser.library.filter {$0 != story}
+            } else {
+                // Appending current story to current user's library array
+                currentUser.library.append(story!)
+            }
+            // Saving user with the updated library
+            currentUser.save {
+                [weak self] result in
+                
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let user):
+                        print("User saved! \(user)")
+                    case .failure(let error):
+                        print("Save Error: \(error)")
+                    }
+                }
+            }
+            // Reloading table view to update addLibrary button/gesture image
+            tableView.reloadTable()
+            
+            // Change add library button based on whether current story is in the library or not
+        }
     }
 }
--- MyArchive/MyArchive/Read/ReadViewController.swift
+++ MyArchive/MyArchive/Read/ReadViewController.swift
@@ -39,3 +39,4 @@ extension ReadViewController: UITableViewDataSource {
     
     
 }
+
